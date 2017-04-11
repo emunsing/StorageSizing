97e2646cc18483bf1995be98908cf46b10161401
emunsing <e.munsing@berkeley.edu>
2017-04-11 11:38:31 -0700

    Intermediate result files for each worker process

---
+++ efficiencySweep.py
@@ -0,0 +1,192 @@
+from cylp.cy import CyClpSimplex
+from cylp.py.modeling.CyLPModel import CyLPArray
+
+import pandas as pd
+import numpy as np
+import scipy.sparse as sps1
+from copy import deepcopy
+from datetime import *
+import dateutil
+from dateutil import parser, relativedelta
+import pytz
+import sys, os
+
+
+from simulationFunctions import *
+
+import sys  # This is necessary for printing updates within a code block, via sys.stdout.flush()
+import time # Use time.sleep(secs) to sleep a process if needed
+
+print("Current environment directory:" + sys.prefix)
+print("System version: "+sys.version)
+
+import warnings
+warnings.simplefilter(action = "ignore", category = FutureWarning)
+
+
+try:
+    fname = os.environ['INPUTFILE']
+except KeyError:
+    fname = "inputData/pricedata_LMP_100.csv" # Only 100 nodes    
+
+APNode_Prices = pd.read_csv( fname, header=0,index_col=0)#,nrows=10)
+APNode_Prices.columns = pd.DatetimeIndex(APNode_Prices.columns,tz=dateutil.tz.tzutc())  # Note: This will be in UTC time. Use .tz_localize(pytz.timezone('America/Los_Angeles')) if a local time zone is desired- but note that this will 
+timestep = relativedelta.relativedelta(APNode_Prices.columns[2],APNode_Prices.columns[1])
+delta_T = timestep.hours  # Time-step in hours
+
+## Deal with NaN prices
+# Drop nodes which are above a cutoff
+goodNodes = (APNode_Prices.isnull().sum(axis=1) < (0.02 * APNode_Prices.shape[1])) # True if node is less than x% NaN values
+APNode_Prices = APNode_Prices[goodNodes]
+# Interpolate remaining NaNs
+APNode_Prices.interpolate(method='linear',axis=1)
+print("Finished Loading Data")
+sys.stdout.flush()
+
+def efficiencySweep(thisSlice):
+    # Simulation parameters
+    # myEfficiencies = [0.6,0.8,0.9]
+
+    pid = multiprocessing.current_process().pid
+    myEfficiencies = np.arange(0.4,1.01,0.02)
+    reservoirSize=1
+    E_min = 0
+    E_max = 1
+    # The 1-hour system will be SOC constrained, rather than power constrained. We accordingly don't worry about P_max and P_min
+
+    lastEfficiency = 0  # This is used to track whether the efficiency has switched
+    storagePrice = 0 * simulationYears # Amortized cost of storage
+    myLength = thisSlice.shape[1]
+
+
+    # Result dataframe: Size, kwhPassed, and profits for each node, at each efficiency (columns)
+    resultIndex = pd.MultiIndex.from_product([thisSlice.index,['cycleCount','storageProfit']])
+    results = pd.DataFrame(index = resultIndex, columns=myEfficiencies)
+    powerOut = pd.DataFrame(index = thisSlice.index, columns = thisSlice.columns)
+
+    # Build basic model, with everything except the state transition constraints
+    # For each node,
+    #  Set the cost function to be the prices for that period
+
+    # For each efficiency,
+    #  if the new efficiency is not the old efficiency:
+    #    Add the state transition constraints with name 'chargeCons'
+    #  Run the simulation
+    #  Remove the state transition constraint
+
+    model = CyClpSimplex()
+    model.logLevel = 0
+    x_var = model.addVariable('x',myLength*3+2)
+    h_constant = sps.hstack( [1, sps.coo_matrix((1, myLength*3+1))] ) # Force h to be a specific size:         
+    (A,b) = createABineq_noPowerConstraint(myLength, E_min, E_max)
+    model.addConstraint(h_constant * x_var == reservoirSize,'fixedSize')
+    model.addConstraint(         A * x_var <= b.toarray(),  'inequalities')
+
+
+    #### LOOP THROUGH nodes
+    for i in range(thisSlice.shape[0]):
+        # Define cost function
+        myNodeName = thisSlice.index[i]
+        energyPrice = thisSlice.loc[myNodeName,:] / 1000.0 # Price $/kWh as array
+        c = np.concatenate([[storagePrice],[0]*(myLength+1),energyPrice,energyPrice],axis=0)  # No cost for storage state; charged for what we consume, get paid for what we discharge
+        c_clp = CyLPArray(c)
+        model.objective = c_clp * x_var
+
+        for eff_round in myEfficiencies:
+
+            if eff_round != lastEfficiency:  # If we just switched nodes (and not efficiencies) don't bother updating efficiencies
+                try:
+                    model.removeConstraint('equalities')
+                except:
+                    pass
+                (eff_in, eff_out) = [np.sqrt(eff_round)] *2  # Properly account for the round trip efficiency of storage
+                (A_eq, b_eq) = createABeq(myLength, delta_T, eff_in, eff_out)
+                model.addConstraint(A_eq * x_var == b_eq.toarray(),'equalities')
+
+            model.primal()  # Solve
+
+            x = model.primalVariableSolution['x']
+            results.loc[(myNodeName,'storageProfit'),eff_round] = np.dot(-c, x)   #Calculate profits at optimal storage level
+            c_grid = x[2+myLength : 2+myLength*2]
+            results.loc[(myNodeName,'cycleCount'),   eff_round] = sum(c_grid)*eff_in # net kWh traveled
+
+            if round(eff_round,2) == 0.9:
+                powerOut.loc[myNodeName,:] = x[2+myLength : 2+myLength*2] + x[2+myLength*2 : 2+myLength*3]
+
+            lastEfficiency = eff_round
+
+        # Done with the loop; reverse the efficiency set and move on to the next node
+        myEfficiencies = myEfficiencies[::-1]
+
+        if (i+1)%50==0:
+        	results.to_csv( 'Data/efficiencyResults_pid'+str(pid)+'temp.csv' )
+        	powerOut.to_csv('Data/efficiencyPower_pid'  +str(pid)+'temp.csv' )
+
+    return (results,powerOut,pid)
+
+## CUSTOM START/END DATE
+startDate = parser.parse('01/01/12 00:00')  # year starts at 2013-01-01 00:00:00
+
+try:
+    endDate=parser.parse(os.environ['ENDDATE'])
+except KeyError:
+    endDate=parser.parse('01/31/12 23:00') # year ends at 2013-12-31 23:00:00
+
+startDate = pytz.timezone('America/Los_Angeles').localize(startDate).astimezone(pytz.utc)
+endDate = pytz.timezone('America/Los_Angeles').localize(endDate).astimezone(pytz.utc)
+
+# ## FULL DATASET
+# startDate = APNode_Prices.columns.values[ 0].astype('M8[m]').astype('O') # Convert to datetime, not timestamp
+# endDate   = APNode_Prices.columns.values[-1].astype('M8[m]').astype('O')
+# startDate = pytz.utc.localize(startDate)
+# endDate   = pytz.utc.localize(endDate)
+
+timespan = relativedelta.relativedelta(endDate +timestep, startDate)
+simulationYears = timespan.years + timespan.months/12. + timespan.days/365. + timespan.hours/8760.  # Leap years will be slightly more than a year, and that's ok.
+
+startNode = 0
+
+try:
+    stopNode = int(os.environ['STOPNODE'])
+    print(stopNode)
+except KeyError:
+    stopNode  = 15 # if set to zero, then will loop through all nodes
+
+if ((stopNode == 0)|(stopNode > APNode_Prices.shape[0])): stopNode = APNode_Prices.shape[0]
+thisSlice = APNode_Prices.ix[startNode:stopNode,startDate:endDate]
+
+import multiprocessing
+
+# Split dataset into roughly even chunks
+j = min(multiprocessing.cpu_count(),10)
+# chunksize = (APNode_Prices.shape[0]/j)+1
+# splitFrames = [df for g,df in APNode_Prices.groupby(np.arange(APNode_Prices.shape[0])//chunksize)]
+chunksize = (thisSlice.shape[0]/j)+1
+splitFrames = [df for g,df in thisSlice.groupby(np.arange(thisSlice.shape[0])//chunksize)]
+
+print("Entering the pool... bye-bye!")
+solverStartTime = time.time()
+
+pool = multiprocessing.Pool(processes = j)
+resultList = pool.map(efficiencySweep,splitFrames) # Each worker returns a tuple of (result,PowerOut,pid)
+
+(resultFrames, powerOutputs, pids) = zip(*resultList)
+
+results = pd.concat(resultFrames).sort_index()
+powerResults = pd.concat(powerOutputs).sort_index()
+
+profitDf = results.loc[(slice(None),'storageProfit'),:].reset_index(level=1,drop=True)
+cycleDf  = results.loc[(slice(None),'cycleCount'),:].reset_index(level=1,drop=True)
+
+profitDf.to_csv('Data/kwhValue_step_02.csv')
+cycleDf.to_csv('Data/cycleCount_step_02.csv')
+powerResults.to_csv('Data/powerOutput_90pct.csv')
+
+for pid in pids:
+    try:
+    	os.remove('Data/efficiencyResults_pid'+str(pid)+'temp.csv')
+        os.remove('Data/efficiencyPower_pid'  +str(pid)+'temp.csv')
+    except OSError:
+        pass  # We probably didn't have enough datapoints to make this relevant
+
+print('Total function call time: %.3f seconds' % (time.time() - solverStartTime))
--- simulationFunctions.py
+++ simulationFunctions.py
@@ -166,6 +166,185 @@ def createABMatrices( Length, delta_t, eff_in, eff_out, P_min, P_max, E_min, E_m
     
     return (A, b, A_eq, B_eq)
 
+def createABineq( Length, P_min, P_max, E_min, E_max):
+    # Returns (A,b) inequality matrices, i.e. A*x <= b
+
+    ## Inequality Constraints
+    # A*x <= b
+    # Each block must have width Length*3+2 and height Length+1
+    # A = [E_1  ; E_2  ; C_1 ;  C_2  ; D_1  ; D_2]
+    # OR
+    # A = [E_1  ; -1* E_1; C_1; -1*C_1; D_1  ; -1*D_1]
+    # b = [E_max;  E_min; C_max; C_min; D_max; D_min]
+
+    # H positivity constraints
+    # -h <=0
+    H_0 = sps.hstack([ -1, sps.coo_matrix((1, Length*3+1)) ] )
+    B_0 = 0
+
+    # E_max Constraints
+    # h * -e_max + e_i <= 0
+    # h * e_min  - e_i <= 0
+    E_1 = sps.hstack( [-E_max * numpy.ones((Length+1, 1)), 
+                       sps.eye(Length+1),
+                       sps.coo_matrix((Length+1,Length*2)) ])
+    #  [   h      E_i     C_i    D_i  ]
+    #  [-e_max  1 0 0 0  0 0 0  0 0 0 ;
+    #   -e_max  0 1 0 0  0 0 0  0 0 0 ;
+    #   -e_max  0 0 1 0  0 0 0  0 0 0 ;
+    #   -e_max  0 0 0 1  0 0 0  0 0 0 ];
+    B_1 = sps.coo_matrix((Length+1,1))
+
+    # E_min Constraints
+    E_2 = sps.hstack( [ E_min * numpy.ones((Length+1, 1)),
+                       -1 * sps.eye(Length+1),
+                       sps.coo_matrix((Length+1,Length*2)) ])
+    B_2 = sps.coo_matrix((Length+1,1))
+
+    # Charge Power Constraints
+    # 0 <= c_i <= pmax     for all i
+    # C max Constraints
+    C_1 = sps.hstack( [ sps.coo_matrix((Length,Length+2)), sps.eye(Length), sps.coo_matrix((Length,Length)) ] )
+    #  [h   E_i     C_i    D_i  ]
+    #   0  0 0 0 0  1 0 0  0 0 0 ;
+    #   0  0 0 0 0  0 1 0  0 0 0 ;
+    #   0  0 0 0 0  0 0 1  0 0 0 ];
+    B_3 = P_max * numpy.ones((Length,1))
+
+    #C Min Constraints
+    C_2 = -1 * C_1
+    B_4 = sps.coo_matrix((Length,1))
+
+
+    # Discharge Power constraints
+    #  pmin <= d_i <= 0     for all i
+    # D max Constraints
+    D_1 = sps.hstack( [sps.coo_matrix((Length,Length*2 +2)), sps.eye(Length) ])
+    #  [h    E_i     C_i    D_i  ]
+    #   0  0 0 0 0  0 0 0  1 0 0 ;
+    #   0  0 0 0 0  0 0 0  0 1 0 ;
+    #   0  0 0 0 0  0 0 0  0 0 1 ];
+    B_5 = sps.coo_matrix((Length,1))
+
+    #D Min Constraints
+    D_2 = -1 * D_1
+    B_6 = -1* P_min * numpy.ones((Length,1))
+
+    #Combining Matricies
+    A = sps.vstack( [H_0, E_1 , E_2 , C_1 , C_2 , D_1 , D_2] )
+    b = sps.vstack( [B_0, B_1 , B_2 , B_3 , B_4 , B_5 , B_6] )
+
+    return (A,b)
+
+def createABineq_noPowerConstraint( Length, E_min, E_max):
+    # Returns (A,b) inequality matrices, i.e. A*x <= b
+
+    ## Inequality Constraints
+    # A*x <= b
+    # Each block must have width Length*3+2 and height Length+1
+    # A = [E_1  ; E_2  ; C_1 ;  C_2  ; D_1  ; D_2]
+    # OR
+    # A = [E_1  ; -1* E_1; C_1; -1*C_1; D_1  ; -1*D_1]
+    # b = [E_max;  E_min; C_max; C_min; D_max; D_min]
+
+    # H positivity constraints
+    # -h <=0
+    H_0 = sps.hstack([ -1, sps.coo_matrix((1, Length*3+1)) ] )
+    B_0 = 0
+
+    # E_max Constraints
+    # h * -e_max + e_i <= 0
+    # h * e_min  - e_i <= 0
+    E_1 = sps.hstack( [-E_max * numpy.ones((Length+1, 1)), 
+                       sps.eye(Length+1),
+                       sps.coo_matrix((Length+1,Length*2)) ])
+    #  [   h      E_i     C_i    D_i  ]
+    #  [-e_max  1 0 0 0  0 0 0  0 0 0 ;
+    #   -e_max  0 1 0 0  0 0 0  0 0 0 ;
+    #   -e_max  0 0 1 0  0 0 0  0 0 0 ;
+    #   -e_max  0 0 0 1  0 0 0  0 0 0 ];
+    B_1 = sps.coo_matrix((Length+1,1))
+
+    # E_min Constraints
+    E_2 = sps.hstack( [ E_min * numpy.ones((Length+1, 1)),
+                       -1 * sps.eye(Length+1),
+                       sps.coo_matrix((Length+1,Length*2)) ])
+    B_2 = sps.coo_matrix((Length+1,1))
+
+    # Charge Power Constraints
+    # 0 <= c_i    for all i
+    # C max Constraints
+    C_3 = -1 * sps.hstack( [ sps.coo_matrix((Length,Length+2)), sps.eye(Length), sps.coo_matrix((Length,Length)) ] )
+    #  [h   E_i     C_i    D_i  ]
+    #   0  0 0 0 0  1 0 0  0 0 0 ;
+    #   0  0 0 0 0  0 1 0  0 0 0 ;
+    #   0  0 0 0 0  0 0 1  0 0 0 ];
+    # B_3 = P_max * numpy.ones((Length,1))
+
+    #C Min Constraints
+    B_3 = sps.coo_matrix((Length,1))
+
+
+    # Discharge Power constraints
+    #  d_i <= 0     for all i
+    # D max Constraints
+    D_4 = sps.hstack( [sps.coo_matrix((Length,Length*2 +2)), sps.eye(Length) ])
+    #  [h    E_i     C_i    D_i  ]
+    #   0  0 0 0 0  0 0 0  1 0 0 ;
+    #   0  0 0 0 0  0 0 0  0 1 0 ;
+    #   0  0 0 0 0  0 0 0  0 0 1 ];
+    B_4 = sps.coo_matrix((Length,1))
+
+    #Combining Matricies
+    A = sps.vstack( [H_0, E_1 , E_2 , C_3 , D_4] )
+    b = sps.vstack( [B_0, B_1 , B_2 , B_3 , B_4] )
+
+    return (A,b)
+
+
+def createABeq( Length, delta_t, eff_in, eff_out):
+    # Returns (A_eq, b_eq) matrices s.t. A_eq * x == b at optimality
+    
+    # Equality constraints:
+    # e_0 = 0
+    # e_i - e_(i-1) - (n_in * c_i)*delta_T + (1/n_out * d_i)*delta_t = 0
+
+    E_init = 0
+    
+    ## Equality Constraints
+    # Control variables:
+    # Length of one variable for each time step
+
+    #  E(0) = E_init
+    #  E(k+1) - E(k) + nT*Ps(k) = 0
+
+    # [E(0) -----E(N) |C(1)...C(N) | D(1)...D(N)]
+
+    # A_eq = [A_eq0, A_eq1 ,  A_eq_2   ,   A_eq3    ]
+    # [0  1  0  0  0  0  0   0  0  0   0   0   0   0;
+    #  0 -1  1  0  0  0 -nT  0  0  0  -1/nT 0  0   0;
+    #  0  0 -1  1  0  0  0 -nT  0  0   0 -1/nT 0   0;
+    #  0  0  0 -1  1  0  0  0  -nT 0   0   0 -1/nT 0;
+    #  0  0  0  0 -1  1  0  0  0  -nT  0   0   0 -1/nT];
+
+    # A_eq = [A_eq0, A_eq1, A_eq_2, A_eq3]
+
+    A_eq_0 = sps.coo_matrix((Length+1,1))
+
+    A_eq_1 = sps.eye(Length+1) - sps.eye(Length+1,k=-1) # Identity - off-diagonal term
+
+    A_eq_2 = sps.vstack( [sps.coo_matrix((1,Length)), 
+                          -1* eff_in * delta_t * sps.eye(Length)])
+
+    A_eq_3 = sps.vstack( [sps.coo_matrix((1,Length)),
+                          -1* delta_t / eff_out * sps.eye(Length)])
+    
+    A_eq = sps.hstack( [A_eq_0, A_eq_1, A_eq_2, A_eq_3])
+
+    B_eq = sps.vstack( [ E_init, sps.coo_matrix((Length,1)) ])
+
+    return (A_eq, B_eq)
+
 def nan_helper(y):
     """Helper to handle indices and logical indices of NaNs.
     Input:
